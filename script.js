a6251387c12f9fe9b60cf0cfe1ad931000c17cc2
aceciliiiasaab <ana.saab.morais@escola.pr.gov.br>
2026-09-25 09:29:48 -0300

    lllkfgvnvn

---
+++ script.js
@@ -0,0 +1,6 @@
+let btmAjuda = document.querySelect(".botao-ajuda");
+let btnFechar = document.querySelect(".botao-fechar");
+let modal = document.querySelect(".modal-fundo");
+
+btnAjuda.addEventListener("click", abreModal);
+btnAjuda.addEventListener("click", fechaModal);
